02eb40c558cca889e96aa973f6f5dfca99c9a620
Rikey Chen <rikey.chen16@gmail.com>
2019-04-22 15:04:13 -0700

    Add #index, #create, #truck_params

--- app/controllers/api/trucks_controller.rb
+++ app/controllers/api/trucks_controller.rb
@@ -1,2 +1,20 @@
 class TrucksController < ApplicationController
+  def index
+    @trucks = Trucks.all
+    render :index
+  end
+
+  def create
+    @truck = Truck.new(truck_params)
+
+    if truck.save
+      render :index
+    else
+      render json: @truck.errors.full_messages, status: 422
+    end
+  end
+
+  def truck_params
+    params.require(:truck).permit(:name, :start_time, :end_time)
+  end
 end

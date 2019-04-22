047b8432658afddc6e4088a5f8f76a3111adc1a3
Rikey Chen <rikey.chen16@gmail.com>
2019-04-21 23:50:33 -0700

    Add validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -12,14 +12,8 @@
 #  truck_id   :integer
 #
 
-# Read about fixtures at http://api.rubyonrails.org/classes/ActiveRecord/FixtureSet.html
+class Job < ApplicationRecord
+  validates :name, :move_date, :start_time, :end_time, :truck_id, presence: true
 
-# This model initially had no columns defined. If you add columns to the
-# model remove the '{}' from the fixture names and add the columns immediately
-# below each fixture, per the syntax in the comments below
-#
-one: {}
-# column: value
-#
-two: {}
-# column: value
+  belongs_to :truck
+end

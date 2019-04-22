d02eb5aa06fab21fc5bb9292425379067ea4cb26
Rikey Chen <rikey.chen16@gmail.com>
2019-04-22 16:09:11 -0700

    Update #available_trucks to check job within truck hours

--- app/models/job.rb
+++ app/models/job.rb
@@ -22,8 +22,14 @@ class Job < ApplicationRecord
       .includes(:jobs)
 
     trucks = trucks.select do |truck|
+      truck_start = truck.start_time.hour
+      truck_end = truck.end_time.hour
+      next if (
+        !start_time.hour.between?(truck_start, truck_end) || !end_time.hour.between?(truck_start, truck_end)
+      )
+
       truck.jobs.all? do |job|
-        job.start_time.hour > end_time.hour || job.end_time.hour < start_time.hour
+        (job.start_time.hour > end_time.hour || job.end_time.hour < start_time.hour)
       end
     end
 

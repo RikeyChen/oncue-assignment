7a143ffbc8f708e531594d24d3314c23f562c8f0
Rikey Chen <rikey.chen16@gmail.com>
2019-04-22 14:59:08 -0700

    Add #create

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,6 +1,17 @@
 class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
+    if @job.available_trucks.length > 0
+      chosen_truck_id = rand(@job.available_trucks.length)
+
+      @job.truck_id = chosen_truck_id
+    end
+
+    if @job.save
+      render 'api/trucks/index'
+    else
+      render json: @job.errors.full_messages, status: 422
+    end
   end
 
   def job_params

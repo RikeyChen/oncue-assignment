e08ece2182ef4fb4393a66699371484114c948a3
Rikey Chen <rikey.chen16@gmail.com>
2019-04-22 17:57:02 -0700

    Add trucks and jobs reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import trucksReducer from './trucks_reducer';
+import jobsReducer from './jobs_reducer';
 
-const rootReducer = (oldState = {}, action) => oldState;
+const rootReducer = combineReducers({
+  trucks: trucksReducer,
+  jobs: jobsReducer,
+});
 
 export default rootReducer;

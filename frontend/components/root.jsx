c41321aaaed7b0df73a90a21a0cff1353e18dbe4
Rikey Chen <rikey.chen16@gmail.com>
2019-04-23 13:49:37 -0700

    Add createTruckContainer and CreateJobContainer

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import TrucksIndexContainer from "./trucks/trucks_index_container";
+import TrucksIndexContainer from './trucks/trucks_index_container';
+import CreateTruckContainer from './trucks/create_truck_container';
+import CreateJobContainer from './jobs/create_job_container';
 
 const Root = ({ store }) => (
   <Provider store={store}>
+    <CreateTruckContainer />
+    <CreateJobContainer />
     <TrucksIndexContainer />
   </Provider>
 );

ae7422e8982b8610a0630765f2e4acb053e3dc05
Rikey Chen <rikey.chen16@gmail.com>
2019-04-23 13:47:20 -0700

    Add time conversion to format for JS milliseconds

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,13 +1,19 @@
 class Api::JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
-    if @job.available_trucks.length > 0
-      chosen_truck_id = rand(@job.available_trucks.length)
 
+    @job.start_time = Time.at(params[:job][:start_time].to_i)
+    @job.end_time = Time.at(params[:job][:end_time].to_i)
+
+    available_trucks = @job.available_trucks
+    if available_trucks.length > 0
+      chosen_truck_idx = rand(available_trucks.length)
+      chosen_truck_id = available_trucks[chosen_truck_idx].id
       @job.truck_id = chosen_truck_id
     end
 
     if @job.save
+      @trucks = Truck.all
       render 'api/trucks/index'
     else
       render json: @job.errors.full_messages, status: 422

6ef736fddb918e82f4b8d7e0c8f58229813d0669
Rikey Chen <rikey.chen16@gmail.com>
2019-04-23 13:50:43 -0700

    Add convertTime

--- frontend/components/trucks/trucks_detail.jsx
+++ frontend/components/trucks/trucks_detail.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
 const TrucksDetail = ({ truck, jobs }) => {
+  const convertTime = (hour) => {
+    if (hour < 12) {
+      return `${hour}am`;
+    }
+
+    if (hour === 12) {
+      return '12pm';
+    }
+
+    return `${hour - 12}pm`;
+  };
+
   const truckJobs = jobs.map((job) => {
-    const startTime = job.start_time;
-    const endTime = job.end_time;
+    const startTime = convertTime(new Date(job.start_time).getHours());
+    const endTime = convertTime(new Date(job.end_time).getHours());
     return (
-      <h2>{`${job.name}, ${job.move_date}, ${startTime} - ${endTime}`}</h2>
+      <h4>{`${job.name}, ${job.move_date}, ${startTime} - ${endTime}`}</h4>
     );
   });
   return (
     <>
-      <h1>
+      <h2>
         {`${truck.name} assignments`}
-      </h1>
+      </h2>
       {truckJobs}
     </>
   );
